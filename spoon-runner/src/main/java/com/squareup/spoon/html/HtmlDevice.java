2edb0d8c28ef767a165208a4b118747fc03b7e48
kortchang <toanyone19980217@gmail.com>
2020-03-18 16:55:08 +0800

    😎Split subtitle

--- spoon-runner/src/main/java/com/squareup/spoon/html/HtmlDevice.java
+++ spoon-runner/src/main/java/com/squareup/spoon/html/HtmlDevice.java
@@ -13,131 +13,131 @@ import java.util.Map;
 import static com.squareup.spoon.DeviceTestResult.Status;
 import static java.util.stream.Collectors.toList;
 
-/** Model for representing a {@code device.html} page. */
+/**
+ * Model for representing a {@code device.html} page.
+ */
 final class HtmlDevice {
-  static HtmlDevice from(String serial, DeviceResult result, File output) {
-    List<TestResult> testResults = new ArrayList<>();
-    int testsPassed = 0;
-    for (Map.Entry<DeviceTest, DeviceTestResult> entry : result.getTestResults().entrySet()) {
-      DeviceTestResult testResult = entry.getValue();
-      testResults.add(TestResult.from(serial, entry.getKey(), testResult, output));
-      if (testResult.getStatus() != Status.FAIL) {
-        testsPassed += 1;
-      }
-    }
+    static HtmlDevice from(String serial, DeviceResult result, File output) {
+        List<TestResult> testResults = new ArrayList<>();
+        int testsPassed = 0;
+        for (Map.Entry<DeviceTest, DeviceTestResult> entry : result.getTestResults().entrySet()) {
+            DeviceTestResult testResult = entry.getValue();
+            testResults.add(TestResult.from(serial, entry.getKey(), testResult, output));
+            if (testResult.getStatus() != Status.FAIL) {
+                testsPassed += 1;
+            }
+        }
 
-    int testsRun = result.getTestResults().size();
-    int testsFailed = testsRun - testsPassed;
-    String totalTestsRun = "😎 " + testsRun + " test" + (testsRun != 1 ? "s" : "");
-    DeviceDetails details = result.getDeviceDetails();
-    String title = (details != null) ? details.getName().replace("_", " ") : serial;
-    title = "📱" + title;
+        int testsRun = result.getTestResults().size();
+        int testsFailed = testsRun - testsPassed;
 
-    List<HtmlUtils.ExceptionInfo> exceptions = result.getExceptions()
-        .stream()
-        .map(HtmlUtils::processStackTrace)
-        .collect(toList());
+        DeviceDetails details = result.getDeviceDetails();
+        String title = (details != null) ? details.getName().replace("_", " ") : serial;
+        title = "📱" + title ;
 
-    StringBuilder subtitle1 = new StringBuilder();
-    subtitle1.append(totalTestsRun).append(" run");
-    if (testsPassed > 0) {
-      subtitle1.append("/<br/>🟩 ")
-              .append(testsPassed)
-              .append(" Passing")
-              .append("  ")
-              .append("🟥 ")
-              .append(testsFailed)
-              .append(" Failing")
-              .append("/<br/>⏱ ")
-              .append(HtmlUtils.humanReadableDuration(result.getDuration()));
-    }
-    subtitle1.append(" at ")
-        .append(HtmlUtils.dateToString(result.getStarted()));
+        String totalTestsRun = "😎 " + testsRun + " test" + (testsRun != 1 ? "s" : "") + " run";
 
-    String subtitle2 = HtmlUtils.deviceDetailsToString(details);
+        List<HtmlUtils.ExceptionInfo> exceptions = result.getExceptions()
+                .stream()
+                .map(HtmlUtils::processStackTrace)
+                .collect(toList());
 
-    return new HtmlDevice(serial, title, subtitle1.toString(), subtitle2, testResults, exceptions);
-  }
 
-  public final String serial;
-  public final String title;
-  public final String subtitle1;
-  public final String subtitle2;
-  public final List<TestResult> testResults;
-  public final boolean hasExceptions;
-  public final List<HtmlUtils.ExceptionInfo> exceptions;
+        String subtitle1TestPass = String.valueOf(testsPassed);
+        String subtitle1TestFail = String.valueOf(testsFailed);
+        String subtitle1Duration = HtmlUtils.humanReadableDuration(result.getDuration()) + " at " + HtmlUtils.dateToString(result.getStarted());
 
-  HtmlDevice(String serial, String title, String subtitle1, String subtitle2,
-      List<TestResult> testResults, List<HtmlUtils.ExceptionInfo> exceptions) {
-    this.serial = serial;
-    this.title = title;
-    this.subtitle1 = subtitle1;
-    this.subtitle2 = subtitle2;
-    this.testResults = testResults;
-    this.hasExceptions = !exceptions.isEmpty();
-    this.exceptions = exceptions;
-  }
+        String subtitle2 = HtmlUtils.deviceDetailsToString(details);
 
-  static final class TestResult implements Comparable<TestResult> {
-    static TestResult from(String serial, DeviceTest test, DeviceTestResult result, File output) {
-      String className = test.getClassName();
-      String methodName = test.getMethodName().replace("_" ," ");
-      String classSimpleName = HtmlUtils.getClassSimpleName(className);
-      String testId = HtmlUtils.testClassAndMethodToId(className, methodName);
-      String status = HtmlUtils.getStatusCssClass(result);
-      List<HtmlUtils.Screenshot> screenshots = result.getScreenshots()
-          .stream()
-          .map(screenshot -> HtmlUtils.getScreenshot(screenshot, output))
-          .collect(toList());
-      List<HtmlUtils.SavedFile> files = result.getFiles()
-          .stream()
-          .map(file -> HtmlUtils.getFile(file, output))
-          .collect(toList());
-      String animatedGif = HtmlUtils.createRelativeUri(result.getAnimatedGif(), output);
-      HtmlUtils.ExceptionInfo exception = HtmlUtils.processStackTrace(result.getException());
-      return new TestResult(serial, className, methodName, classSimpleName, methodName,
-          testId, status, screenshots, animatedGif, exception, files);
+        return new HtmlDevice(serial, title, subtitle2, totalTestsRun, subtitle1TestPass, subtitle1TestFail,subtitle1Duration, testResults, exceptions);
     }
 
     public final String serial;
-    public final String className;
-    public final String methodName;
-    public final String classSimpleName;
-    public final String prettyMethodName;
-    public final String testId;
-    public final String status;
-    public final boolean hasScreenshots;
-    public final List<HtmlUtils.Screenshot> screenshots;
-    public final List<HtmlUtils.SavedFile> files;
-    public final boolean hasFiles;
-    public final String animatedGif;
-    public final HtmlUtils.ExceptionInfo exception;
+    public final String title;
+    public final String subtitle1TestPass;
+    public final String subtitle1TestFail;
+    public final String subtitle1TotalTestRunAndDevice;
+    public final String subtitle1Duration;
+    public final String subtitle2;
+    public final List<TestResult> testResults;
+    public final boolean hasExceptions;
+    public final List<HtmlUtils.ExceptionInfo> exceptions;
 
-    TestResult(String serial, String className, String methodName, String classSimpleName,
-        String prettyMethodName, String testId, String status,
-        List<HtmlUtils.Screenshot> screenshots, String animatedGif,
-        HtmlUtils.ExceptionInfo exception, List<HtmlUtils.SavedFile> files) {
-      this.serial = serial;
-      this.className = className;
-      this.methodName = methodName;
-      this.classSimpleName = classSimpleName;
-      this.prettyMethodName = prettyMethodName;
-      this.testId = testId;
-      this.status = status;
-      this.hasScreenshots = !screenshots.isEmpty();
-      this.screenshots = screenshots;
-      this.animatedGif = animatedGif;
-      this.exception = exception;
-      this.files = files;
-      this.hasFiles = !files.isEmpty();
+    HtmlDevice(String serial, String title, String subtitle2, String subtitle1TotalTestRunAndDevice, String subtitle1TestPass, String subtitle1TestFail,
+               String subtitle1Duration, List<TestResult> testResults, List<HtmlUtils.ExceptionInfo> exceptions) {
+        this.serial = serial;
+        this.title = title;
+        this.subtitle2 = subtitle2;
+        this.subtitle1TotalTestRunAndDevice = subtitle1TotalTestRunAndDevice;
+        this.subtitle1TestPass = subtitle1TestPass;
+        this.subtitle1TestFail = subtitle1TestFail;
+        this.subtitle1Duration = subtitle1Duration;
+        this.testResults = testResults;
+        this.hasExceptions = !exceptions.isEmpty();
+        this.exceptions = exceptions;
     }
 
-    @Override public int compareTo(TestResult other) {
-      int classComparison = className.compareTo(other.className);
-      if (classComparison != 0) {
-        return classComparison;
-      }
-      return methodName.compareTo(other.methodName);
+    static final class TestResult implements Comparable<TestResult> {
+        static TestResult from(String serial, DeviceTest test, DeviceTestResult result, File output) {
+            String className = test.getClassName();
+            String methodName = test.getMethodName().replace("_", " ");
+            String classSimpleName = HtmlUtils.getClassSimpleName(className);
+            String testId = HtmlUtils.testClassAndMethodToId(className, methodName);
+            String status = HtmlUtils.getStatusCssClass(result);
+            List<HtmlUtils.Screenshot> screenshots = result.getScreenshots()
+                    .stream()
+                    .map(screenshot -> HtmlUtils.getScreenshot(screenshot, output))
+                    .collect(toList());
+            List<HtmlUtils.SavedFile> files = result.getFiles()
+                    .stream()
+                    .map(file -> HtmlUtils.getFile(file, output))
+                    .collect(toList());
+            String animatedGif = HtmlUtils.createRelativeUri(result.getAnimatedGif(), output);
+            HtmlUtils.ExceptionInfo exception = HtmlUtils.processStackTrace(result.getException());
+            return new TestResult(serial, className, methodName, classSimpleName, methodName,
+                    testId, status, screenshots, animatedGif, exception, files);
+        }
+
+        public final String serial;
+        public final String className;
+        public final String methodName;
+        public final String classSimpleName;
+        public final String prettyMethodName;
+        public final String testId;
+        public final String status;
+        public final boolean hasScreenshots;
+        public final List<HtmlUtils.Screenshot> screenshots;
+        public final List<HtmlUtils.SavedFile> files;
+        public final boolean hasFiles;
+        public final String animatedGif;
+        public final HtmlUtils.ExceptionInfo exception;
+
+        TestResult(String serial, String className, String methodName, String classSimpleName,
+                   String prettyMethodName, String testId, String status,
+                   List<HtmlUtils.Screenshot> screenshots, String animatedGif,
+                   HtmlUtils.ExceptionInfo exception, List<HtmlUtils.SavedFile> files) {
+            this.serial = serial;
+            this.className = className;
+            this.methodName = methodName;
+            this.classSimpleName = classSimpleName;
+            this.prettyMethodName = prettyMethodName;
+            this.testId = testId;
+            this.status = status;
+            this.hasScreenshots = !screenshots.isEmpty();
+            this.screenshots = screenshots;
+            this.animatedGif = animatedGif;
+            this.exception = exception;
+            this.files = files;
+            this.hasFiles = !files.isEmpty();
+        }
+
+        @Override
+        public int compareTo(TestResult other) {
+            int classComparison = className.compareTo(other.className);
+            if (classComparison != 0) {
+                return classComparison;
+            }
+            return methodName.compareTo(other.methodName);
+        }
     }
-  }
 }
--- spoon-runner/src/main/java/com/squareup/spoon/html/HtmlIndex.java
+++ spoon-runner/src/main/java/com/squareup/spoon/html/HtmlIndex.java
@@ -6,6 +6,7 @@ import com.squareup.spoon.DeviceResult;
 import com.squareup.spoon.DeviceTest;
 import com.squareup.spoon.DeviceTestResult;
 import com.squareup.spoon.SpoonSummary;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -16,134 +17,138 @@ import java.util.Map;
 import static com.squareup.spoon.DeviceTestResult.Status;
 import static java.util.stream.Collectors.toList;
 
-/** Model for representing the {@code index.html} page. */
+/**
+ * Model for representing the {@code index.html} page.
+ */
 final class HtmlIndex {
-  static HtmlIndex from(SpoonSummary summary) {
-    int testsRun = 0;
-    int totalSuccess = 0;
-    List<Device> devices = new ArrayList<>();
-    for (Map.Entry<String, DeviceResult> result : summary.getResults().entrySet()) {
-      devices.add(Device.from(result.getKey(), result.getValue()));
-      Map<DeviceTest, DeviceTestResult> testResults = result.getValue().getTestResults();
-      testsRun += testResults.size();
-      for (Map.Entry<DeviceTest, DeviceTestResult> entry : testResults.entrySet()) {
-        if (entry.getValue().getStatus() != Status.FAIL) {
-          totalSuccess += 1;
+    static HtmlIndex from(SpoonSummary summary) {
+        int testsRun = 0;
+        int totalSuccess = 0;
+        List<Device> devices = new ArrayList<>();
+        for (Map.Entry<String, DeviceResult> result : summary.getResults().entrySet()) {
+            devices.add(Device.from(result.getKey(), result.getValue()));
+            Map<DeviceTest, DeviceTestResult> testResults = result.getValue().getTestResults();
+            testsRun += testResults.size();
+            for (Map.Entry<DeviceTest, DeviceTestResult> entry : testResults.entrySet()) {
+                if (entry.getValue().getStatus() != Status.FAIL) {
+                    totalSuccess += 1;
+                }
+            }
         }
-      }
+
+        Collections.sort(devices);
+
+        int totalFailure = testsRun - totalSuccess;
+
+        int deviceCount = summary.getResults().size();
+        IRemoteAndroidTestRunner.TestSize testSize = summary.getTestSize();
+        String started = HtmlUtils.dateToString(summary.getStarted());
+        String totalTestsRun = "😎 " + testsRun + (testSize != null ? " " + testSize.name().toLowerCase() : "")
+                + " test" + (testsRun != 1 ? "s" : "");
+        String totalDevices = " " + deviceCount + " device" + (deviceCount != 1 ? "s" : "");
+
+        StringBuilder subtitle = new StringBuilder();
+
+        String subtitleTotalTestsRunAndDevice = totalTestsRun + " run across " + totalDevices;
+
+        String subtitle1TestPass = String.valueOf(totalSuccess);
+        String subtitle1TestFail = String.valueOf(totalFailure);
+        String subtitle1Duration = HtmlUtils.humanReadableDuration(summary.getDuration()) + " at " + HtmlUtils.dateToString(summary.getStarted());
+
+        return new HtmlIndex(summary.getTitle(), subtitleTotalTestsRunAndDevice, subtitle1TestPass, subtitle1TestFail, subtitle1Duration, devices);
     }
 
-    Collections.sort(devices);
+    public final String title;
+    public final String subtitleTotalTestsRunAndDevice;
+    public final String subtitle1TestPass;
+    public final String subtitle1TestFail;
+    public final String subtitle1Duration;
+    public final List<Device> devices;
 
-    int totalFailure = testsRun - totalSuccess;
-
-    int deviceCount = summary.getResults().size();
-    IRemoteAndroidTestRunner.TestSize testSize = summary.getTestSize();
-    String started = HtmlUtils.dateToString(summary.getStarted());
-    String totalTestsRun = "😎 " + testsRun + (testSize != null ? " " + testSize.name().toLowerCase() : "")
-            + " test" + (testsRun != 1 ? "s" : "");
-    String totalDevices = " " + deviceCount + " device" + (deviceCount != 1 ? "s" : "");
-
-    StringBuilder subtitle = new StringBuilder();
-    subtitle.append(totalTestsRun).append(" run across ").append(totalDevices);
-    if (testsRun > 0) {
-      subtitle.append("<br>🟩 ")
-              .append(totalSuccess)
-              .append(" Passing")
-              .append("  ")
-              .append("🟥 ")
-              .append(totalFailure)
-              .append(" Failing")
-              .append("/<br/>⏱ ")
-              .append(HtmlUtils.humanReadableDuration(summary.getDuration()));
-    }
-    subtitle.append(" at ").append(started);
-
-    return new HtmlIndex(summary.getTitle(), subtitle.toString(),  devices);
-  }
-
-  public final String title;
-  public final String subtitle;
-  public final List<Device> devices;
-
-  HtmlIndex(String title, String subtitle, List<Device> devices) {
-    this.title = title;
-    this.subtitle = subtitle;
-    this.devices = devices;
-  }
-
-  static final class Device implements Comparable<Device> {
-    static Device from(String serial, DeviceResult result) {
-      List<TestResult> testResults = result.getTestResults()
-          .entrySet()
-          .stream()
-          .map(entry -> TestResult.from(serial, entry.getKey(), entry.getValue()))
-          .collect(toList());
-      DeviceDetails details = result.getDeviceDetails();
-      String name = (details != null) ? details.getName() : serial;
-      boolean executionFailed = testResults.isEmpty() && !result.getExceptions().isEmpty();
-      return new Device(serial, name, testResults, executionFailed);
+    HtmlIndex(String title, String subtitleTotalTestsRunAndDevice, String subtitle1TestPass, String subtitle1TestFail, String subtitle1Duration, List<Device> devices) {
+        this.title = title;
+        this.subtitleTotalTestsRunAndDevice = subtitleTotalTestsRunAndDevice;
+        this.subtitle1TestPass = subtitle1TestPass;
+        this.subtitle1TestFail = subtitle1TestFail;
+        this.subtitle1Duration = subtitle1Duration;
+        this.devices = devices;
     }
 
-    public final String serial;
-    public final String name;
-    public final List<TestResult> testResults;
-    public final boolean executionFailed;
-    public final int testCount;
+    static final class Device implements Comparable<Device> {
+        static Device from(String serial, DeviceResult result) {
+            List<TestResult> testResults = result.getTestResults()
+                    .entrySet()
+                    .stream()
+                    .map(entry -> TestResult.from(serial, entry.getKey(), entry.getValue()))
+                    .collect(toList());
+            DeviceDetails details = result.getDeviceDetails();
+            String name = (details != null) ? details.getName() : serial;
+            boolean executionFailed = testResults.isEmpty() && !result.getExceptions().isEmpty();
+            return new Device(serial, name, testResults, executionFailed);
+        }
 
-    Device(String serial, String name, List<TestResult> testResults, boolean executionFailed) {
-      this.serial = serial;
-      this.name = name;
-      this.testResults = testResults;
-      this.testCount = testResults.size();
-      this.executionFailed = executionFailed;
+        public final String serial;
+        public final String name;
+        public final List<TestResult> testResults;
+        public final boolean executionFailed;
+        public final int testCount;
+
+        Device(String serial, String name, List<TestResult> testResults, boolean executionFailed) {
+            this.serial = serial;
+            this.name = name;
+            this.testResults = testResults;
+            this.testCount = testResults.size();
+            this.executionFailed = executionFailed;
+        }
+
+        @Override
+        public int compareTo(@NotNull Device other) {
+            if (name == null && other.name == null) {
+                return serial.compareTo(other.serial);
+            }
+            if (name == null) {
+                return 1;
+            }
+            if (other.name == null) {
+                return -1;
+            }
+            return name.compareTo(other.name);
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name : serial;
+        }
     }
 
-    @Override public int compareTo(@NotNull Device other) {
-      if (name == null && other.name == null) {
-        return serial.compareTo(other.serial);
-      }
-      if (name == null) {
-        return 1;
-      }
-      if (other.name == null) {
-        return -1;
-      }
-      return name.compareTo(other.name);
-    }
+    static final class TestResult implements Comparable<TestResult> {
+        static TestResult from(String serial, DeviceTest test, DeviceTestResult testResult) {
+            String className = test.getClassName();
+            String methodName = test.getMethodName();
+            String classSimpleName = HtmlUtils.getClassSimpleName(className);
+            String testId = HtmlUtils.testClassAndMethodToId(className, methodName);
+            String status = HtmlUtils.getStatusCssClass(testResult);
+            return new TestResult(serial, classSimpleName, methodName, testId, status);
+        }
 
-    @Override public String toString() {
-      return name != null ? name : serial;
-    }
-  }
+        public final String serial;
+        public final String classSimpleName;
+        public final String prettyMethodName;
+        public final String testId;
+        public final String status;
 
-  static final class TestResult implements Comparable<TestResult> {
-    static TestResult from(String serial, DeviceTest test, DeviceTestResult testResult) {
-      String className = test.getClassName();
-      String methodName = test.getMethodName();
-      String classSimpleName = HtmlUtils.getClassSimpleName(className);
-      String testId = HtmlUtils.testClassAndMethodToId(className, methodName);
-      String status = HtmlUtils.getStatusCssClass(testResult);
-      return new TestResult(serial, classSimpleName, methodName, testId, status);
-    }
+        TestResult(String serial, String classSimpleName, String prettyMethodName, String testId,
+                   String status) {
+            this.serial = serial;
+            this.classSimpleName = classSimpleName;
+            this.prettyMethodName = prettyMethodName;
+            this.testId = testId;
+            this.status = status;
+        }
 
-    public final String serial;
-    public final String classSimpleName;
-    public final String prettyMethodName;
-    public final String testId;
-    public final String status;
-
-    TestResult(String serial, String classSimpleName, String prettyMethodName, String testId,
-        String status) {
-      this.serial = serial;
-      this.classSimpleName = classSimpleName;
-      this.prettyMethodName = prettyMethodName;
-      this.testId = testId;
-      this.status = status;
+        @Override
+        public int compareTo(@NotNull TestResult other) {
+            return 0;
+        }
     }
-
-    @Override public int compareTo(@NotNull TestResult other) {
-      return 0;
-    }
-  }
 }
